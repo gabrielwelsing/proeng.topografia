853f2b8e3b08c36c06243894ca3231bb9024230e
gabrielwelsing <gabriel.welsing@gmail.com>
2026-02-06 15:36:19 -0300

    versao 4.0

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack: (config) => {
-      // Isso diz pro Next.js ignorar módulos de servidor quando estiver no navegador
-      config.resolve.alias.canvas = false;
-      config.resolve.alias.encoding = false;
-      return config;
-    },
-  };
-  
-  export default nextConfig;
+  webpack: (config) => {
+    // Isso diz pro Next.js ignorar módulos de servidor quando estiver no navegador
+    config.resolve.alias.canvas = false;
+    config.resolve.alias.encoding = false;
+    return config;
+  },
+};
+
+export default nextConfig;
